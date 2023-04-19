2550e406f79bb8c13113cf1709774c88ea48658e
hlbrand <hlbrand2995@gmail.com>
2023-04-20 06:46:10 +0700

    08 Home page. 001 Home main structure

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,9 @@
+import styles from '../styles/Home.module.scss';
 import Header from '@/components/header';
 import Footer from '@/components/footer';
 import axios from 'axios';
 import { useSession, signIn, signOut } from 'next-auth/react';
+import Main from '@/components/home/main';
 
 export default function Home({ country }) {
   const { data: session } = useSession();
@@ -9,7 +11,11 @@ export default function Home({ country }) {
   return (
     <div>
       <Header country={country} />
-      {session ? 'You are logged in' : 'You are not logged in'}
+      <div className={styles.home}>
+        <div className={styles.container}>
+          <Main />
+        </div>
+      </div>
       <Footer country={country} />
     </div>
   );
