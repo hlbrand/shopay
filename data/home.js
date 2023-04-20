c99a90e105ac576659542f54a1c0b437d74f00c2
hlbrand <hlbrand2995@gmail.com>
2023-04-20 15:31:47 +0700

    08 Home page. 004 Home offers main

---
+++ data/home.js
@@ -0,0 +1,26 @@
+export const offersArray = [
+  {
+    image:
+      'https://ae01.alicdn.com/kf/Sbb42d72feeb94421b875766dafb07dcfM/Luxury-Tree-of-Life-Zircon-Necklace-for-Women-Gold-plated-Charm-Big-Tree-Stainless-Steel-Pendant.jpg_Q90.jpg_.webp',
+    price: '1.11',
+    discount: '63',
+  },
+  {
+    image:
+      'https://ae01.alicdn.com/kf/H65e2ca02276b43afb578dceb6ac3b920X/Gold-Silver-Color-Metal-Butterfly-Ear-Clips-Without-Piercing-For-Women-Sparkling-Zircon-Ear-Cuff-Clip.jpg_Q90.jpg_.webp',
+    price: '0.46',
+    discount: '25',
+  },
+  {
+    image:
+      'https://ae01.alicdn.com/kf/Sb2998931851e44d5a9bb4173856a4f06z/New-Korean-Arrival-Metal-Trendy-Fresh-Lovely-Sweet-Grey-Leaf-Stud-Earrings-For-Women-2023-Fashion.jpg_Q90.jpg_.webp',
+    price: '0.45',
+    discount: '70',
+  },
+  {
+    image:
+      'https://ae01.alicdn.com/kf/Sb2998931851e44d5a9bb4173856a4f06z/New-Korean-Arrival-Metal-Trendy-Fresh-Lovely-Sweet-Grey-Leaf-Stud-Earrings-For-Women-2023-Fashion.jpg_Q90.jpg_.webp',
+    price: '0.45',
+    discount: '70',
+  },
+];
