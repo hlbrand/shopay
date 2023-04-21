2551b36759f48720d5238d877e6b8a1ac94941da
hlbrand <hlbrand2995@gmail.com>
2023-04-21 10:30:21 +0700

    08 Home page. 008 Home main header

--- components/home/main/index.js
+++ components/home/main/index.js
@@ -1,13 +1,15 @@
+import Link from 'next/link';
 import Menu from './Menu';
 import User from './User';
 import Offers from './offers';
 import styles from './styles.module.scss';
 import MainSwiper from './swiper';
+import Header from './Header';
 
 export default function Main() {
   return (
     <div className={styles.main}>
-      <div className={styles.header}>header</div>
+      <Header />
       <Menu />
       <MainSwiper />
       <Offers />
